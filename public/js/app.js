e639fc4e48bad8befc3815554f039a39e39f8029
Vladimir988 <optimist0706@gmail.com>
2023-05-29 14:47:06 +0300

    update form dinamic

--- public/js/app.js
+++ public/js/app.js
@@ -5354,7 +5354,8 @@ __webpack_require__.r(__webpack_exports__);
   name: 'IndexComponent',
   data: function data() {
     return {
-      people: null
+      people: null,
+      editPersonId: null
     };
   },
   methods: {
@@ -5363,6 +5364,12 @@ __webpack_require__.r(__webpack_exports__);
       axios.get('/api/people', {}).then(function (responce) {
         _this.people = responce.data;
       });
+    },
+    changeEditPersonId: function changeEditPersonId(id) {
+      this.editPersonId = id;
+    },
+    isEdit: function isEdit(id) {
+      return this.editPersonId === id;
     }
   },
   mounted: function mounted() {
@@ -5582,14 +5589,65 @@ var render = function render() {
     staticClass: "container mt-5"
   }, [_c("h2", [_vm._v("Index Component")]), _vm._v(" "), _c("table", {
     staticClass: "table mt-5"
-  }, [_vm._m(0), _vm._v(" "), _c("tbody", _vm._l(_vm.people, function (person) {
-    return _c("tr", [_c("th", [_vm._v(_vm._s(person.id))]), _vm._v(" "), _c("td", [_vm._v(_vm._s(person.name))]), _vm._v(" "), _c("td", [_vm._v(_vm._s(person.age))]), _vm._v(" "), _c("td", [_vm._v(_vm._s(person.job))])]);
-  }), 0)])]);
+  }, [_vm._m(0), _vm._v(" "), _c("tbody", [_vm._l(_vm.people, function (person) {
+    return [_c("tr", [_c("th", [_vm._v(_vm._s(person.id))]), _vm._v(" "), _c("td", [_vm._v(_vm._s(person.name))]), _vm._v(" "), _c("td", [_vm._v(_vm._s(person.age))]), _vm._v(" "), _c("td", [_vm._v(_vm._s(person.job))]), _vm._v(" "), _c("td", [_c("a", {
+      staticClass: "btn btn-success",
+      attrs: {
+        href: "#"
+      },
+      on: {
+        click: function click($event) {
+          $event.preventDefault();
+          return _vm.changeEditPersonId(person.id);
+        }
+      }
+    }, [_vm._v("Edit")])])]), _vm._v(" "), _c("tr", {
+      "class": _vm.isEdit(person.id) ? "" : "d-none"
+    }, [_c("th"), _vm._v(" "), _vm._m(1, true), _vm._v(" "), _vm._m(2, true), _vm._v(" "), _vm._m(3, true), _vm._v(" "), _c("td", [_c("a", {
+      staticClass: "btn btn-success",
+      attrs: {
+        href: "#"
+      },
+      on: {
+        click: function click($event) {
+          $event.preventDefault();
+          return _vm.changeEditPersonId(null);
+        }
+      }
+    }, [_vm._v("Update")])])])];
+  })], 2)])]);
 };
 var staticRenderFns = [function () {
   var _vm = this,
     _c = _vm._self._c;
-  return _c("thead", [_c("tr", [_c("th"), _vm._v(" "), _c("th", [_vm._v("Name")]), _vm._v(" "), _c("th", [_vm._v("Age")]), _vm._v(" "), _c("th", [_vm._v("Job")])])]);
+  return _c("thead", [_c("tr", [_c("th"), _vm._v(" "), _c("th", [_vm._v("Name")]), _vm._v(" "), _c("th", [_vm._v("Age")]), _vm._v(" "), _c("th", [_vm._v("Job")]), _vm._v(" "), _c("th", [_vm._v("Edit")])])]);
+}, function () {
+  var _vm = this,
+    _c = _vm._self._c;
+  return _c("td", [_c("input", {
+    staticClass: "form-control",
+    attrs: {
+      type: "text"
+    }
+  })]);
+}, function () {
+  var _vm = this,
+    _c = _vm._self._c;
+  return _c("td", [_c("input", {
+    staticClass: "form-control",
+    attrs: {
+      type: "number"
+    }
+  })]);
+}, function () {
+  var _vm = this,
+    _c = _vm._self._c;
+  return _c("td", [_c("input", {
+    staticClass: "form-control",
+    attrs: {
+      type: "text"
+    }
+  })]);
 }];
 render._withStripped = true;
 
--- resources/js/components/IndexComponent.vue
+++ resources/js/components/IndexComponent.vue
@@ -4,20 +4,31 @@
 
         <table class="table mt-5">
             <thead>
-            <tr>
-                <th></th>
-                <th>Name</th>
-                <th>Age</th>
-                <th>Job</th>
-            </tr>
+                <tr>
+                    <th></th>
+                    <th>Name</th>
+                    <th>Age</th>
+                    <th>Job</th>
+                    <th>Edit</th>
+                </tr>
             </thead>
             <tbody>
-            <tr v-for="person in people"> <!-- v-if="person.age > 20" --> <!-- personAgeFilter -->
-                <th>{{ person.id }}</th>
-                <td>{{ person.name }}</td>
-                <td>{{ person.age }}</td>
-                <td>{{ person.job }}</td>
-            </tr>
+                <template v-for="person in people"> <!-- v-if="person.age > 20" --> <!-- personAgeFilter -->
+                    <tr>
+                        <th>{{ person.id }}</th>
+                        <td>{{ person.name }}</td>
+                        <td>{{ person.age }}</td>
+                        <td>{{ person.job }}</td>
+                        <td><a href="#" @click.prevent="changeEditPersonId(person.id)" class="btn btn-success">Edit</a></td>
+                    </tr>
+                    <tr :class="isEdit(person.id) ? '' : 'd-none'">
+                        <th></th>
+                        <td><input type="text" class="form-control"></td> <!-- value="{{ person.name }}" -->
+                        <td><input type="number" class="form-control"></td> <!-- value="{{ person.age }}" -->
+                        <td><input type="text" class="form-control"></td> <!-- value="{{ person.job }}" -->
+                        <td><a href="#" @click.prevent="changeEditPersonId(null)" class="btn btn-success">Update</a></td>
+                    </tr>
+                </template>
             </tbody>
         </table>
     </div>
@@ -28,7 +39,8 @@ export default {
     name: 'IndexComponent',
     data() {
         return {
-            people: null
+            people: null,
+            editPersonId: null
         };
     },
     methods: {
@@ -39,6 +51,12 @@ export default {
             .then(responce => {
                 this.people = responce.data
             });
+        },
+        changeEditPersonId(id) {
+            this.editPersonId = id;
+        },
+        isEdit(id) {
+            return this.editPersonId === id;
         }
     },
     mounted() {
